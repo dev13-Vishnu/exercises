4381488213bdd441c6d886bfe2c6e37c81572f7d
Vishnu P S <vishnu13three@gamil.com>
2025-01-19 09:46:05 +0530

    To-Do list bare bones

--- src/exercises/ToDoLIst.jsx
+++ src/exercises/ToDoLIst.jsx
@@ -1,5 +1,38 @@
-
+import React,{useState} from "react";
 function ToDoList() {
-    
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask,setNewTask] = useState("");
+
+    const handleAddTask =() => {
+        const updatedTask = [...tasks,newTask];
+        setTasks(updatedTask);
+        setNewTask("");
+    }
+    const handleDelete = (index) =>{
+        setTasks(tasks.filter((_,i) => i !== index));
+    }
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <input
+             type="text"
+             value={newTask}
+             onChange={(e) => setNewTask(e.target.value)} 
+             placeholder="Enter a Task..."
+             />
+            <button
+             onClick={handleAddTask}>Add Task
+            </button>
+            <ul>
+            {tasks.map((task,index) => <li 
+                key={index}>
+                {task}
+                <button onClick={() =>handleDelete(index)}>Delete</button>
+                </li> )}
+
+            </ul>
+        </div>
+    )
 }
 export default ToDoList;
