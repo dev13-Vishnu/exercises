ee90a941a10165ee69b736a78a75c3a6b92edc04
Vishnu P S <vishnu13three@gamil.com>
2025-01-20 14:55:24 +0530

    Theme toggle added complete css

--- src/exercises/ThemeToggle.jsx
+++ src/exercises/ThemeToggle.jsx
@@ -1,16 +1,24 @@
-import React, {useState} from'react';
+import React, { useState } from "react";
 
-function ThemeToggle(){
-    const [isDarkTheme, setIsDarkTheme] =useState(false);
+function ThemeToggle() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-    const toggleTheme = () => {
-        setIsDarkTheme((prevTheme) => !prevTheme);
-    };
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
 
-    return(
-        <div className={isDarkTheme?'dark-theme':'light-theme'}>
-
-        </div>
-    )
+  return (
+    <div className={isDarkTheme ? "dark-theme" : "light-theme"}>
+      <header className="header">
+        <h1>Theme Toggle Example</h1>
+        <button className="theme-toggle-btn" onClick={toggleTheme}>
+          Switch to {isDarkTheme ? "Light" : "Dark"} Theme
+        </button>
+      </header>
+      <main className="content">
+        <p> This is a simple example  of a light/dark theme in React.</p>
+      </main>
+    </div>
+  );
 }
 export default ThemeToggle;
